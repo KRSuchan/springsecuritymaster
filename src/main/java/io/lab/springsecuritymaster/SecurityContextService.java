ba3d45db913e342f18cfa814b4a0cda8395e3d7d
KRSuchan <lsc1814@naver.com>
2024-08-21 20:58:21 +0900

    SecurityContextService

---
+++ src/main/java/io/lab/springsecuritymaster/SecurityContextService.java
@@ -0,0 +1,17 @@
+package io.lab.springsecuritymaster;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityContextService {
+
+    public void securityContext() {
+        SecurityContext securityContext = SecurityContextHolder.getContextHolderStrategy().getContext();
+        Authentication authentication = securityContext.getAuthentication();
+        System.out.println("Authentication: " + authentication);
+    }
+    
+}
