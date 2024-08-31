a3e0999a255da3f6ea8f667a61df932ed2cb95eb
KRSuchan <lsc1814@naver.com>
2024-08-31 14:24:51 +0900

    default branch, SecurityConfig의 requestMatchers 추가

--- src/main/java/io/lab/springsecuritymaster/SecurityConfig.java
+++ src/main/java/io/lab/springsecuritymaster/SecurityConfig.java
@@ -18,6 +18,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
         ;
