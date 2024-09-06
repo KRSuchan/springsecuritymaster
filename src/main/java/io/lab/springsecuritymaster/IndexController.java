ea680dbe95e6c735d6d4876413dffd7d064f309b
KRSuchan <lsc1814@naver.com>
2024-09-06 14:38:10 +0900

    HttpSecurity.authorizeHttpRequests

--- src/main/java/io/lab/springsecuritymaster/IndexController.java
+++ src/main/java/io/lab/springsecuritymaster/IndexController.java
@@ -1,53 +1,50 @@
 package io.lab.springsecuritymaster;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.CurrentSecurityContext;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
 public class IndexController {
 
-    @GetMapping("/")
-    public Authentication index(Authentication authentication) {
-        return authentication;
+    @GetMapping("/user")
+    public String user() {
+        return "user";
     }
 
-    @GetMapping("/loginPage")
-    public String loginPage() {
-        return "loginPage";
+    @GetMapping("/myPage/points")
+    public String myPage() {
+        return "myPage";
     }
 
-    @GetMapping("/home")
-    public String home() {
-        return "home";
+    @GetMapping("/manager")
+    public String manager() {
+        return "manager";
     }
 
-    @GetMapping("/anonymous")
-    public String anonymous() {
-        return "anonymous";
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
     }
 
-    @GetMapping("/authentication")
-    public String authentication(Authentication authentication) {
-        if (authentication instanceof AnonymousAuthenticationToken) {
-            return "anonymous";
-        } else {
-            return "not anonymous";
-        }
+    @GetMapping("/admin/payment")
+    public String adminPayment() {
+        return "adminPayment";
     }
 
-    @GetMapping("/anonymousContext")
-    public String anonymousContext(@CurrentSecurityContext SecurityContext context) {
-        return context.getAuthentication().getName();
+    @GetMapping("/resource/address_01")
+    public String address_01() {
+        return "address_01";
     }
 
-    @GetMapping("/logoutSuccess")
-    public String logoutSuccess() {
-        return "logoutSuccess";
+    @GetMapping("/resource/address01")
+    public String address01() {
+        return "address01";
     }
+
+    @PostMapping("/post")
+    public String post() {
+        return "post";
+    }
+
 }
---
+++ src/main/java/io/lab/springsecuritymaster/ViewController.java
@@ -0,0 +1,12 @@
+package io.lab.springsecuritymaster;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
