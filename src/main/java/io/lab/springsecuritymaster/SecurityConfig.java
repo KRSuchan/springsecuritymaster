a8417a23f37e0e928a1ac68da7f184683421f2cd
KRSuchan <lsc1814@naver.com>
2024-09-10 21:14:05 +0900

    roleHierarchy

--- src/main/java/io/lab/springsecuritymaster/SecurityConfig.java
+++ src/main/java/io/lab/springsecuritymaster/SecurityConfig.java
@@ -1,33 +1,62 @@
 package io.lab.springsecuritymaster;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl.fromHierarchy;
+
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
     @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (webSecurity) -> {
+            webSecurity.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+        };
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/user").hasRole("USER")
+                        .requestMatchers("/db").hasRole("DB")
+                        .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
         ;
+
         return http.build();
     }
-    
+
+    @Bean
+    public RoleHierarchyImpl roleHierarchy() {
+        return fromHierarchy(
+                """
+                        ROLE_ADMIN > ROLE_DB
+                        ROLE_DB > ROLE_USER
+                        ROLE_USER > ROLE_ANONYMOUS
+                        """
+        );
+    }
+
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
-        return new InMemoryUserDetailsManager(user);
+        UserDetails db = User.withUsername("db").password("{noop}1111").roles("DB").build();
+        UserDetails admin = User.withUsername("admin").password("{noop}1111").roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user, db, admin);
     }
 }
