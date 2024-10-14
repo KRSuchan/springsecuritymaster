3c2eb17f2e9881f26cb7c10addbf56b5ace7c4af
KRSuchan <lsc1814@naver.com>
2024-10-14 19:57:55 +0900

    로그인 페이지 만들기

--- src/main/java/io/lab/springsecuritymaster/controller/HomeController.java
+++ src/main/java/io/lab/springsecuritymaster/controller/HomeController.java
@@ -1,4 +1,4 @@
-package io.lab.springsecuritymaster;
+package io.lab.springsecuritymaster.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
