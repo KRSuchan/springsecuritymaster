5e2dfcdff1eef7f54a77827896b72b2365081e47
KRSuchan <lsc1814@naver.com>
2024-10-16 19:48:10 +0900

    FormAuthenticationProvider

---
+++ src/main/java/io/lab/springsecuritymaster/security/exception/SecretException.java
@@ -0,0 +1,10 @@
+package io.lab.springsecuritymaster.security.exception;
+
+
+import org.springframework.security.core.AuthenticationException;
+
+public class SecretException extends AuthenticationException {
+    public SecretException(String message) {
+        super(message);
+    }
+}
